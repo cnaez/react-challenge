a0d760b80d4979e9299f015866d654db221affda
Sina <cnaesmz@gmail.com>
2021-11-11 04:10:55 +0330

    bug fix

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,35 +1,29 @@
-import React from 'react'
-import { SketchOutlined, ArrowLeftOutlined } from '@ant-design/icons'
-import './Hero.css'
+import React from "react";
+import { SketchOutlined, ArrowLeftOutlined } from "@ant-design/icons";
+import "./Hero.css";
 
 const Hero = (props) => {
   return (
-    <div className='container'>
-      <div className='thumbnail'>
-        <img className='heroPic' src={props.pic} />
-        <div className='lable'>باز</div>
-        <div className='lable2'>A+ در حد نو</div>
+    <div className="container">
+      <div className="thumbnail">
+        <img className="heroPic" src={props.pic} alt="product" />
+        <div className="lable">باز</div>
+        <div className="lable2">A+ در حد نو</div>
       </div>
-      <div className='heroText'>
+      <div className="heroText">
         <div>
-          <div className='heroTitle'>
-            {props.title}
-          </div>
-          <div>
-            {props.lastSeen}
-          </div>
+          <div className="heroTitle">{props.title}</div>
+          <div>{props.lastSeen}</div>
         </div>
-      
-        <a href='#'>
+
+        <a href="#">
           <SketchOutlined />
-
           کارنامه
-
           <ArrowLeftOutlined />
         </a>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Hero
+export default Hero;
